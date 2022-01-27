dc0b0882bf2c5f98b59014876b92f6f9da393432
유지언 <yje941@naver.com>
2022-01-27 15:55:35 +0900

    SWEA

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,21 +1,12 @@
 ## 2056번
 
-
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+
 for test_case in range(1, T + 1):
-    numbers = str(map(int, input()))
-
-# 0. 입력값을 int로 할지 str로 받을지? "str"
-# 1. 유효성을 판단 -- 월과 일을 판단) 월은 index 4,5 일은 6, 7
-    if int(numbers[4:6]) == 1:
-        if 1 <= int(numbers[4:6]) <= 31:
-            answer = numbers
-        else:
-            answer == -1
-
     
+    numbers = list(map(int, input().split()))
+    mid_num = numbers[len(numbers)//2]
+            
+    print(f'{mid_num}') 
 
-# 2. 유효하다면, yyyy/mm/dd 형식
-# 3. 유효하지 않다면, '-1'을 출력해야 함
 
